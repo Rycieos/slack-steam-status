b367a3e29c866763338290035f2ae2a89bca5915
Mark Vander Stel <mvndrstl@gmail.com>
2020-05-25 13:47:32 -0400

    Reference API path from app object
    This is more in line with how the API app should work, instead of
    storing a constant of the path.

--- slack_api.py
+++ slack_api.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
 import asyncio
-from fastapi import FastAPI, Form, Header, HTTPException, Request
+from fastapi import FastAPI, Header, HTTPException, Request
 from fastapi.responses import RedirectResponse
 
 import steam_status.db as db
@@ -10,7 +10,6 @@ import steam_status.steam as steam
 
 app = FastAPI()
 SLACK_SCOPE = "users.profile:write"
-REDIRECT_URI = "/api/slack_after_auth"
 
 with open("steam_status_settings.py") as config:
   exec(config.read(), globals(), globals())
@@ -20,7 +19,6 @@ with open("steam_status_settings.py") as config:
 async def add_user(request: Request,
                    x_slack_request_timestamp: int = Header(...),
                    x_slack_signature: str = Header(...),
-                   #text: str = Form(...),
   ):
 
   if not slack.validate_request(await request.body(), x_slack_request_timestamp,
@@ -42,24 +40,24 @@ async def add_user(request: Request,
 
   return {
     "response_type": "ephemeral",
-    "text": "<{}/api/slack_auth/{}|Click here> to enable Steam status "
-        "syncing".format(HTTP_BASE_URL, steam_id),
+    "text": "<{}{}|Click here> to enable Steam status "
+        "syncing".format(HTTP_BASE_URL, app.url_path_for("auth", steam_id=steam_id)),
   }
 
 # Endpoint for start of oauth dance
 @app.get('/api/slack_auth/{steam_id}')
 def auth(steam_id: str):
   return RedirectResponse("https://slack.com/oauth/v2/authorize?"
-      "client_id={}&user_scope={}&state={}&redirect_uri={}".format(
-      SLACK_APP_ID, SLACK_SCOPE, steam_id, HTTP_BASE_URL + REDIRECT_URI))
+      "client_id={}&user_scope={}&state={}&redirect_uri={}{}".format(
+      SLACK_APP_ID, SLACK_SCOPE, steam_id, HTTP_BASE_URL, app.url_path_for("after_auth")))
 
 # Endpoint for end of oauth dance. Save the user's oauth token
-@app.get(REDIRECT_URI, status_code=201)
+@app.get('/api/slack_after_auth', status_code=201)
 async def after_auth(code: str, state: str):
   steam_id = state
 
   token = await slack.get_oauth_token(SLACK_APP_ID, SLACK_APP_SECRET,
-      code, HTTP_BASE_URL + REDIRECT_URI)
+      code, HTTP_BASE_URL + app.url_path_for("after_auth"))
 
   if not token:
     return "Error: failed oauth dance!"
